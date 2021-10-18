3a63febf3587289515eafce35c2a0b7b3b9a323e
Mohib Khan <mohibk.in@gmail.com>
2021-10-18 22:20:39 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'df968b6ec47f0ba98579dd1e1e540e14'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '25f14f74-a8ac-487a-a3b6-4debb24350f8'
+};
